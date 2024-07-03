cc501e15cdcf5ef773d67421138ce44a577b7918
YamaSomA <poiuytrgh321@gmail.com>
2024-07-03 14:20:02 +0900

    内容を修正

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,12 @@
-const today = new Date();
+let d = new Date();
 
-console.log(today);
+console.log(d.toString());
+
+let year = d.getFullYear();
+let month = d.getMonth() + 1;
+let day = d.getDate();
+let dayofweek = d.getDay();
+
+const dayname = ['日', '月', '火', '水', '木', '金', '土'];
+
+console.log(year + '年' + month + '月' + day + '日' + '[' + dayname[dayofweek] + ']');
